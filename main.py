8b05b59b1b5176d931d3097a4a8aceb0ea6777d6
minhyungPark <puhehe99@gmail.com>
2019-09-24 17:18:32 +0900

    S1P1243014-22 Req 1-5-3

--- main.py
+++ main.py
@@ -30,21 +30,21 @@ def main(self):
     # 데이터를 통한 사전 구성 한다.
     char2idx, idx2char, vocabulary_length = data.load_voc()
     # 훈련 데이터와 테스트 데이터를 가져온다.
-    train_q, train_a, test_q, test_a = None
+    train_q, train_a, test_q, test_a = data.load_data()
 
     # 훈련셋 인코딩 만드는 부분
-    train_input_enc = None
+    train_input_enc = data.enc_processing(train_q)
     # 훈련셋 디코딩 입력 부분
-    train_input_dec = None
+    train_input_dec = data.dec_input_processing(train_q)
     # 훈련셋 디코딩 출력 부분
-    train_target_dec = None
+    train_target_dec = data.dec_target_processing(train_a)
 
     # 평가셋 인코딩 만드는 부분
-    eval_input_enc = None
+    eval_input_enc = data.enc_processing(test_q)
     # 평가셋 인코딩 만드는 부분
-    eval_input_dec = None
+    eval_input_dec = data.dec_input_processing(test_q)
     # 평가셋 인코딩 만드는 부분
-    eval_target_dec = None
+    eval_target_dec = data.dec_target_processing(test_a)
 
     # 현재 경로'./'에 현재 경로 하부에
     # 체크 포인트를 저장한 디렉토리를 설정한다.
@@ -58,7 +58,7 @@ def main(self):
 
     # 에스티메이터 구성한다.
     classifier = tf.estimator.Estimator(
-        model_fn=ml.Model,  # 모델 등록한다.
+        model_fn=ml.model,  # 모델 등록한다.
         model_dir=DEFINES.check_point_path,  # 체크포인트 위치 등록한다.
         params={  # 모델 쪽으로 파라메터 전달한다.
             'embedding_size': DEFINES.embedding_size,
@@ -67,7 +67,7 @@ def main(self):
             'attention_head_size': DEFINES.attention_head_size,
             'learning_rate': DEFINES.learning_rate,  # 학습율 설정한다.
             'vocabulary_length': vocabulary_length,  # 딕셔너리 크기를 설정한다.
-            'embedding_size': DEFINES.embedding_size,  # 임베딩 크기를 설정한다.
+            # 'embedding_size': DEFINES.embedding_size,  # 임베딩 크기를 설정한다.
             'layer_size': DEFINES.layer_size,
             'max_sequence_length': DEFINES.max_sequence_length,
             'xavier_initializer': DEFINES.xavier_initializer
@@ -84,7 +84,7 @@ def main(self):
 
     # 테스트용 데이터 만드는 부분이다.
     # 인코딩 부분 만든다. 테스트용으로 ["가끔 궁금해"] 값을 넣어 형성된 대답과 비교를 한다.
-    predic_input_enc = None
+    predic_input_enc = ["가끔 궁금해"]
     # 학습 과정이 아니므로 디코딩 입력은
     # 존재하지 않는다.(구조를 맞추기 위해 넣는다.)
     predic_input_dec = None
